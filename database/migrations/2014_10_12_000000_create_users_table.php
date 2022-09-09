f328306887aa99c0576b79dbf4f7e2eb638b5038
jigsawsoul <richardclivehowse@gmail.com>
2022-09-09 21:31:19 +0100

    Add user database fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->string('social_avatar')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->nullable();
+            $table->decimal('credit', 8, 2)->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('marketing_email')->nullable()->default(false);
+            $table->boolean('marketing_sms')->nullable()->default(false);
+            $table->json('stripe_identity')->nullable();
+            $table->boolean('bypass_identity')->default(false);
+            $table->boolean('administrator')->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
